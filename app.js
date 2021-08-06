bc3010df9d77114ef0a96b9b4f878728f82c6d51
Sebastian G <gayle054@gmail.com>
2021-08-06 10:06:43 -0700

    crypto tampering

--- app.js
+++ app.js
@@ -9,7 +9,15 @@ class CryptoBlock {
     }
     computeHash(){
         return SHA256(this.index + this.precedingHash + this.timestamp + JSON.stringify(this.data)).toString();
-    }   
+    }  
+    
+    // proofOfWork(difficulty){
+    //     while(this.hash.substring(0, difficulty) !==Array(difficulty + 1).join("0")){
+    //         this.nonce++;
+    //         this.hash = this.computeHash();
+    //     }        
+    // }
+  
 }
 
 
@@ -18,14 +26,15 @@ class CryptoBlockChain {
         this.blockchain = [this.startGenesisBlock()];     
     }
     startGenesisBlock(){
-        return new CryptoBlock(0, "01/01/2020", "Initial Block in the Chain", "0");
+        return new CryptoBlock(0, "01/01/2021", "Initial Block in the Chain", "0");
     }
     obtainLatestBlock(){
         return this.blockchain[this.blockchain.length - 1];
     }
     addNewBlock(newBlock){
         newBlock.precedingHash = this.obtainLatestBlock().hash;
-        newBlock.hash = newBlock.computeHash();        
+        newBlock.hash = newBlock.computeHash();
+        // newBlock.proofOfWork(this.difficulty);          
         this.blockchain.push(newBlock);
     }
 
@@ -33,7 +42,7 @@ class CryptoBlockChain {
     // Checking validity
     for(let i = 1; i < this.block1chain.length; i++) {
         const currentBlock = this.block1chain[i];
-        const nextBlock= this.blockchain[i-1];
+        const nextBlock = this.blockchain[i-1];
     // Checking current blcok hash
     
     if(currentBlock.hash !== currentBlock.computeHash()) {
@@ -52,9 +61,11 @@ class CryptoBlockChain {
 
 let thecoin = new CryptoBlockChain();
 
-    thecoin.addNewBlock(new CryptoBlock(1, "06/04/2021", {sender: "Rabin Yitzack", recipient: "Loyd Eve", quantity: 20}));
+    thecoin.addNewBlock(new CryptoBlock(1, "06/04/2021", {sender: "Jake Pinto", recipient: "Loyd Eve", quantity: 20}));
 
-    thecoin.addNewBlock(new CryptoBlock(2, "07/04/2021", {sender: "Anita Vyona", recipient: "Felix Mush", quantity: 349}));
+    thecoin.addNewBlock(new CryptoBlock(2, "07/04/2021", {sender: "Anita Vyona", recipient: "Bjorn Shevock", quantity: 349}));
+
+    thecoin.addNewBlock(new CryptoBlock(2, "08/05/2021", {sender: "Laura Fintel", recipient: "Nubis Grantwel", quantity: 349}));
 
     console.log(JSON.stringify(thecoin, null, 4));
 
